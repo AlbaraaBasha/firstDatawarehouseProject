fca48633b29ae09c00122ffdd53cdbe4071a8113
AlbaraaBasha <91744549+AlbaraaBasha@users.noreply.github.com>
2026-08-24 00:28:36 +0300

    Modify table definitions and add drop statements

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -14,24 +14,31 @@ cst_gender char,
 cst_create_date date
 );
 
+IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_prd_info;
+GO
 create table bronze.crm_prd_info(
 prd_id int,
 prd_key nvarchar(50),
 prd_nm nvarchar(50),
 prd_cost int,
-prd_line char,
+prd_line nvarchar(50),
 prd_start_dt DATETIME,
 prd_end_dt DATETIME
 );
 
+IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_sales_details;
+GO
 create table bronze.crm_sales_details(
 sls_order_number nvarchar(50),
 sls_prd_key nvarchar(50),
 sls_cust_id int,
-sls_order_id int,
+sls_order_dt int,
 sls_ship_dt int,
 sls_due_dt int,
 sls_sales int,
+sls_quantity int,
 sls_price int
 );
 
