c9b763d810715ef869853a5ed30cd09c76d3fea6
AlbaraaBasha <91744549+AlbaraaBasha@users.noreply.github.com>
2026-08-23 20:55:06 +0300

    Create init_db.sql

---
+++ scripts/init_db.sql
@@ -0,0 +1,14 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+*/
+use master;
+create database DataWarehouse;
+use DataWarehouse;
+create schema bronze;
+go
+create schema silver;
+go
+create schema gold;
+go
