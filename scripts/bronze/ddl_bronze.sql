1aad3b5b13d6fda826d399349dc71c142f5b5a6b
AlbaraaBasha <91744549+AlbaraaBasha@users.noreply.github.com>
2026-08-23 21:17:26 +0300

    Create DDL script for Bronze tables

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,54 @@
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+*/
+
+create table bronze.crm_cust_info(
+cst_id int,
+cst_key nvarchar(20),
+cst_firstname nvarchar(50),
+cst_lastname nvarchar(50),
+cst_marital_status char,
+cst_gender char,
+cst_create_date date
+);
+
+create table bronze.crm_prd_info(
+prd_id int,
+prd_key nvarchar(50),
+prd_nm nvarchar(50),
+prd_cost int,
+prd_line char,
+prd_start_dt DATETIME,
+prd_end_dt DATETIME
+);
+
+create table bronze.crm_sales_details(
+sls_order_number nvarchar(50),
+sls_prd_key nvarchar(50),
+sls_cust_id int,
+sls_order_id int,
+sls_ship_dt int,
+sls_due_dt int,
+sls_sales int,
+sls_price int
+);
+
+CREATE TABLE bronze.erp_loc_a101 (
+    cid    NVARCHAR(50),
+    cntry  NVARCHAR(50)
+);
+
+CREATE TABLE bronze.erp_cust_az12 (
+    cid    NVARCHAR(50),
+    bdate  DATE,
+    gen    NVARCHAR(50)
+);
+
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+    id           NVARCHAR(50),
+    cat          NVARCHAR(50),
+    subcat       NVARCHAR(50),
+    maintenance  NVARCHAR(50)
+);
